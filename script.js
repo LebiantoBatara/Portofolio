f8ff16e5db5aa0aa3e5e188dd10785380d2a288c
Drobot <37899588+LebiantoBatara@users.noreply.github.com>
2026-05-30 11:48:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("themeToggle").addEventListener("click", function () {
-    const html = document.documentElement;
-
-    if (html.getAttribute("data-theme") === "dark") {
-        html.removeAttribute("data-theme");
-    } else {
-        html.setAttribute("data-theme", "dark");
-    }
-});
